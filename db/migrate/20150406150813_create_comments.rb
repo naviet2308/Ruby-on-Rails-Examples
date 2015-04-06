223e93f0ad8de35e66a9f2bc46e9f9531cb9a654
naviet <naviet2308@gmail.com>
2015-04-07 01:02:42 +0700

    handle add & show comment

--- db/migrate/20150406150813_create_comments.rb
+++ db/migrate/20150406150813_create_comments.rb
@@ -3,8 +3,10 @@ class CreateComments < ActiveRecord::Migration
     create_table :comments do |t|
       t.string :commenter
       t.text :body
+ 
+      # this line adds an integer column called `article_id`.
       t.references :article, index: true
-
+ 
       t.timestamps null: false
     end
     add_foreign_key :comments, :articles
